f834767d67ec945ac3f39d317a0228ffe7a1b377
vntero <shin.antero@gmail.com>
2022-06-21 09:50:34 +0200

    wrote test but it hasn't passed just yet

--- server.test.ts
+++ server.test.ts
@@ -1,2 +1,24 @@
+import { send } from 'process'
 import supertest from 'supertest'
 import server from './server'
+
+describe("POST /users", () => {
+
+    describe("given a username and password", () => {
+        // should save the username and password to the db
+        // should respond with a json object containing the user id
+
+        test ("should respond with a 200 status code", async () => {
+            const response = await request(server).post("/users").send({
+                username: "username",
+                password: "password"
+            })
+            expect(response.statusCode).toBe(200)
+        })
+        // should specify the json in the content type header
+    })
+
+    describe("when the username and password are missing", () => {
+        // respond with a status code of 400
+    })
+})
