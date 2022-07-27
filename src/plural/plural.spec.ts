bae32b22fc411f63fff151786fb9a4debf53cb67
vntero <shin.antero@gmail.com>
2022-07-27 08:47:57 +0200

    fixed imports and played around with the test

--- src/plural/plural.spec.ts
+++ src/plural/plural.spec.ts
@@ -1,5 +1,6 @@
-import { plural } from './plural'
+// import { plural } from './plural'
+const plural = require('./plural')
 
 test('Determine if a plural is needed or not', () => {
-    expect(plural(5)).toBe(true)
+    expect(plural(1)).toBe(false)
 })
--- src/plural/plural.ts
+++ src/plural/plural.ts
@@ -1,3 +1,5 @@
-export function plural(n:number):boolean {
+function plural(n) {
     return n < 1 || n > 1 ? true : false
   }
+
+module.exports = plural
