a03fb0f2c92beb3b6f0c5f791671307e155e149a
vntero <shin.antero@gmail.com>
2022-08-03 12:53:40 +0100

    added function that runs through an array and counts the number of x and o

---
+++ src/x-and-o/xoCounter.ts
@@ -0,0 +1,13 @@
+export function xo(str: string) {
+    let x = 0,
+        o = 0;
+
+    for (var i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === "x") {
+            x++;
+        } else if (str[i].toLowerCase() === "o") {
+            o++;
+        }
+    }
+    return x === o;
+}
